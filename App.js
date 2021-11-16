c25fae9a7016e6a032797ac5955626a9087ed70a
Illyas <illyas.deulect@gmail.com>
2021-11-16 19:51:08 +0530

    profile screen on progress

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
+import auth from '@react-native-firebase/auth';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ContextWrapper from './context/ContextWrapper';
+import Profile from './screens/Profile';
 import SignIn from './screens/SignIn';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(currentUser) {
+    setUser(currentUser);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (initializing) return null;
+
   return (
     <ContextWrapper>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="SIGIN" component={SignIn} />
-        </Stack.Navigator>
+        {!user ? (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SIGIN" component={SignIn} />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="PROFILE" component={Profile} />
+          </Stack.Navigator>
+        )}
       </NavigationContainer>
     </ContextWrapper>
   );
